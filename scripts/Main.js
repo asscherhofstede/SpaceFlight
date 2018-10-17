bbb87e3b41c9884f3345410196c9020ba77e6071
Kevin <kevin.visser.1995@gmail.com>
2018-10-17 16:29:07 +0200

    Bewegingcode van spaceship in movement.js gezet

--- scripts/Main.js
+++ scripts/Main.js
@@ -26,31 +26,10 @@ window.onload = function () {
     //schipvariabelen
     //var shipChoice = prompt("voer een 1 in voor een generiek ruimteschip, 2 voor een spaceshuttle en 3 voor een vliegtuigje");
     var shipChoice = '1';
-    var rotationSpeed = 0.05; //de snelheid van de rotatie
 
     //gamevars
     var pause = 1;
     var gameSpeed = 0.5;
-
-    //bewegingsvariabele
-    var moveRight = false;
-    var moveLeft = false;
-    var moveUp = false;
-    var moveDown = false;
-    var rotateLeft = false;
-    var rotateRight = false;
-    var rotateUp = false;
-    var rotateDown = false;
-    var rotateYLeft = false;
-    var rotateYRight = false;
-    var rotateZLeft = false;
-    var rotateZRight = false;
-    var curRotLeftRight = 0;
-    var CurRotUpDown = 0;
-    var sideSpeed = 0.5;
-    var curRotY = 0;
-    var curRotZ = 0;
-    var noseTurnSpeed = 0.03;
 
     //spelerscore
     var playerScore = 1;
@@ -167,102 +146,23 @@ window.onload = function () {
         renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
-    
     function onDocumentKeyDown(event) {
-        var keyCode = event.which;
-
-        if (keyCode == 90) {  //z key voor rotatie over rechts
-            rotateRight = true;
-
-        } else if (keyCode == 88) { // x key voor rotatie over links
-            rotateLeft = true;
-
-        } else if (keyCode == 37) { // left key voor bewegen naar links
-            moveLeft = true;
-            rotateLeft = false;
-        } else if (keyCode == 39) { // right key voor bewegen naar rechts
-            rotateRight = false;
-            moveRight = true;
-        } else if (keyCode == 38) { // up key voor naar boven
-            moveUp = true;
-
-        } else if (keyCode == 40) { //down key voor naar beneden
-            moveDown = true;
-        } else if (keyCode == 80) { //down key voor naar pause
-            if (pause == 1) {
-                pause = 0;
-                pauseMusic();
-            } else {
-                pause = 1;
-                resumeMusic();
-            }
+        switch (event.which){
+            case 80:
+                if(pause == 1){
+                    pause = 0;
+                    pauseMusic();
+                } else {
+                    pause = 1;
+                    resumeMusic();
+                }
         }
-    };
-    function onDocumentKeyUp(event) {
-        var keyCode = event.which;
-
-        if (keyCode == 90) {  //z key voor rotatie over rechts
-            //rotateRight = false;
-
-        } else if (keyCode == 88) { // x key voor rotatie over links
-            //rotateLeft = false;
-
-        } else if (keyCode == 37) { // left key voor bewegen naar links
-            moveLeft = false;
-            stabiliseerSchip();
-        } else if (keyCode == 39) { // right key voor bewegen naar rechts
-            moveRight = false;
-            stabiliseerSchip();
-
-        } else if (keyCode == 38) { // up key voor naar boven
-            moveUp = false;
-            stabiliseerSchip();
-        } else if (keyCode == 40) { //down key voor naar beneden
-            moveDown = false;
-            stabiliseerSchip();
-        }
-    };
-
-    function stabiliseerSchip() {
-        if (!(curRotLeftRight > -0.05)) //links
-        {
-            rotateLeft = true;
-        }
-        else if (!(curRotLeftRight < 0.05)) //rechts
-        {
-            rotateRight = true;
-        }
-        if (curRotY > 0.01) {
-            rotateYLeft = true;
-        }
-        if (curRotY < -0.01) {
-            rotateYRight = true;
-        }
-        if( curRotZ < -0.01 )
-        {
-            rotateZRight = true;
-        }
-        if( curRotZ > 0.01 )
-        {
-            rotateZLeft = true;
-        }
-        if(CurRotUpDown > 0.01)
-        {
-            rotateDown = true;
-        }
-        if(CurRotUpDown < -0.01)
-        {
-            rotateUp = true;
-        }
-    }
-
-    
+    };    
 
     function animate() {
         setTimeout(function () {
 
             requestAnimationFrame(animate);
-            //cameraControls.update();
             updateScore();
 
             gameSpeed += 0.00025 * pause;
@@ -286,138 +186,10 @@ window.onload = function () {
                         break;
                 }
             }
-
-            if (moveRight == true) {
-                spaceshipModel.position.z -= sideSpeed * pause; 
-                camera.position.z -= sideSpeed * pause;
-
-                if(curRotLeftRight < 0.5 && curRotLeftRight > -0.6){ //zorgt voor draaiing over eigen as naar rechts
-                    curRotLeftRight += rotationSpeed;
-                    spaceshipModel.rotation.x -= rotationSpeed * pause;
-                }
-                if(curRotLeftRight > 0.45) //als de rotatie over eigen as voldoende is, draai het schip naar rechts
-                {
-                    if(curRotY < 0.3 && curRotY > -0.4){
-                        curRotY += noseTurnSpeed;
-                        spaceshipModel.rotation.y -= noseTurnSpeed * pause;
-                        //spaceshipModel.rotation.z -= noseTurnSpeed * pause;
-                    }
-                    if(curRotZ < 0.3 && curRotZ > -0.4) //test
-                    {
-                        curRotZ += noseTurnSpeed;
-                        spaceshipModel.rotation.z -= noseTurnSpeed * pause;
-                    }
-                }
-            }
-            if (moveLeft == true) {
-                spaceshipModel.position.z += sideSpeed * pause;
-                camera.position.z += sideSpeed * pause;
-
-                if(curRotLeftRight < 0.6 && curRotLeftRight > -0.5){ //zorgt voor draaiing over eigen as naar links
-                    curRotLeftRight -= rotationSpeed;
-                    spaceshipModel.rotation.x += rotationSpeed * pause;
-                }
-                if(curRotLeftRight<-0.45) //als de rotatie over eigen as voldoende is, draai het schip naar links
-                {
-                    if(curRotY < 0.4 && curRotY > -0.3){
-                        curRotY -= noseTurnSpeed;
-                        spaceshipModel.rotation.y += noseTurnSpeed * pause;
-                        //spaceshipModel.rotation.z += noseTurnSpeed * pause;
-                    }
-                    if(curRotZ < 0.4 && curRotZ > -0.3) //test
-                    {
-                        curRotZ += noseTurnSpeed;
-                        spaceshipModel.rotation.z -= noseTurnSpeed * pause;
-                    }
-                }
-            }
-            if (moveUp == true) {
-                spaceshipModel.position.y += 0.2 * pause;
-                camera.position.y += 0.2 * pause;
-                    if(CurRotUpDown < 0.3 && CurRotUpDown > -0.3){
-                        CurRotUpDown += noseTurnSpeed;
-                        spaceshipModel.rotation.z -= noseTurnSpeed * pause;
-                    }
-            }
-            if (moveDown == true) {
-                spaceshipModel.position.y -= 0.2 * pause;
-                camera.position.y -= 0.2 * pause;
-                    if(CurRotUpDown < 0.3 && CurRotUpDown > -0.3){
-                        CurRotUpDown -= noseTurnSpeed;
-                        spaceshipModel.rotation.z += noseTurnSpeed * pause;
-                    }
-            }
-            if (rotateRight == true) { //roteert het schip zijn rol over eigen as terug naar rechtop
-                if(curRotLeftRight > 0.01){
-                    spaceshipModel.rotation.x += 0.05 * pause;
-                    curRotLeftRight -= 0.05;
-                } else {
-                    rotateRight = false;
-                }
-            }
-            if (rotateLeft == true) { //roteert het schip zijn rol over eigen as terug naar rechtop
-                if(curRotLeftRight < -0.01){
-                    spaceshipModel.rotation.x -= 0.05 * pause;
-                    curRotLeftRight += 0.05;
-                } else {
-                    rotateLeft = false;
-                }
-            }
-            if (rotateYLeft == true) {
-                
-                if(curRotY > 0.01){
-                    spaceshipModel.rotation.y += 0.05 * pause;
-                    curRotY -= 0.05;
-                } else {
-                    rotateYLeft = false;
-                }
-            }
-            if (rotateYRight == true) {
-                
-                if(curRotY < -0.01){
-                    spaceshipModel.rotation.y -= 0.05 * pause;
-                    curRotY += 0.05;
-                } else {
-                    rotateYRight = false;
-                }
-            }
-            if (rotateZLeft == true) {
-                
-                if(curRotZ > 0.01){
-                    spaceshipModel.rotation.z += 0.05 * pause;
-                    curRotZ -= 0.05;
-                }else {
-                    rotateZLeft = false;
-                }
-            }
-            if (rotateZRight == true) {
-                
-                if(curRotZ > 0.01){
-                    spaceshipModel.rotation.z -= 0.05 * pause;
-                    curRotZ -= 0.05;
-                }else {
-                    rotateZRight = false;
-                }
-            }
-            if (rotateUp == true) {
-                if(CurRotUpDown < -0.01){
-                    spaceshipModel.rotation.z -= 0.03 * pause;
-                    CurRotUpDown += 0.03;
-                }else{
-                    rotateUp = false;
-                }
-            }
-            if (rotateDown == true) {
-                if(CurRotUpDown > 0.01){
-                    spaceshipModel.rotation.z += 0.03 * pause;
-                    CurRotUpDown -= 0.03;
-                }else{
-                    rotateDown = false;
-                }
-            }
+            AnimateSpaceshipM(spaceshipModel, camera);
+            
         }, 1000 / 60);
-
-        //renderer.render();
+        
         renderer.render(scene, camera);
     }
 
